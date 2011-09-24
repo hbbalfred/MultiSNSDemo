3783defb9c16064fb515d40560e295e667701ea2
Tang Bo Hao <btspoony@gmail.com>
2011-09-24 18:08:53 +0800

    modify config

--- config.js
+++ config.js
@@ -3,29 +3,34 @@
  */
 exports.configApp = function(app,express) {
 	// Database connection
-	app.set('db-uri', 'mongodb://localhost/axegang-dev');
+	app.set('db-uri', 'mongodb://1poqgzuv4spsk:g07ur39pktr@127.0.0.1:20088/2nrcpql4s0jfp');
 
 	// Google Analytics Key
 	app.set('google-analytics-key', 'XXXXXXXX');
 
 	// cdn url config
-	app.set('cdn-url','http://axedomain.bois.com');
+	app.set('cdn-url','http://multisns.cnodejs.net');
 	
 	// App url config
-	app.set('server-url', 'http://axedomain.bois.com');
+	app.set('server-url', 'http://multisns.cnodejs.net');
   
 	// renren app
-	app.set('rr-key', '{key}');
-	app.set('rr-secret', '{secret}');
+	app.set('rr-key', '576ae21a8ee44f48a020dd7c3a8ad04a');
+	app.set('rr-secret', 'f98882c139bf4c76867ce5b0cc3793d8');
   
 	// sina app
-	app.set('sina-key', '{key}');
-	app.set('sina-secret', '{secret}');
+	app.set('sina-key', '971971272');
+	app.set('sina-secret', '26a3a814f45b908dd5e3dd819937a19d');
 };
 
 // SocketIO Configure
 exports.configIO = function(io) {
 	io.set('log level', 4);
-	io.set('transports', ['websocket']);
+	io.set('transports', [
+		'websocket'	
+	  , 'htmlfile'
+	  , 'xhr-polling'
+	  , 'jsonp-polling'
+	]);
 };
 
